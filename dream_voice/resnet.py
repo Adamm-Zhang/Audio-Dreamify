cdcdabdbfeb56ced1bf66e6b4735943028d9782d
Adam Zhang <adamyqz2003@gmail.com>
2025-12-30 23:23:37 -0500

    added transient loss class to resnet model; this is used to ensure rhythm and drum beats stay approx. the same between audio input and transformation

--- dream_voice/resnet.py
+++ dream_voice/resnet.py
@@ -73,3 +73,45 @@ input_tensor = torch.randn(1, 16, 108)  # (Batch, Channels, Time)
 out = model1(input_tensor)
 print(out.shape)  # should be (1, 16, 108)
 
+class transientLoss(nn.Module):
+  def __init__(self, rave_model):
+    super().__init__()
+    self.rave_model = rave_model
+
+    # eval so we dont get random dropout/batchnorm effects on rave; treat musicnet.ts as fixed feature extractor
+    rave_model.eval()
+    
+    # freeze gradients so we dont backprop through rave
+    for param in self.rave_model.parameters():
+      param.requires_grad = False
+    
+  def get_envelope(self, embedding):
+    signal = self.rave_model.decode(embedding)
+    
+    # rave.compute_envelope() might not exist in torchscript file
+    # might be too fast of an envelope sampling window
+    # goal is to blur signal to get transient envelope
+    #envelope = self.rave_model.compute_envelope(signal)
+    rectified = torch.abs(signal)
+    
+    # dont add padding here; might add silence artifacts that confuse model
+    envelope = F.avg_pool1d(rectified, kernel_size=512, stride=256)
+    return envelope
+
+  def forward(self, input_signal_prediction, target_signal):
+    
+    input_envelope = self.get_envelope(input_signal_prediction)
+    
+    # safety; redundant since we fixed rave earlier
+    with torch.no_grad():
+      target_envelope = self.get_envelope(target_signal)
+    
+    # avoid 0 pads as a result of pooling
+    min_length = min(input_envelope.shape[-1], target_envelope.shape[-1])
+    input_envelope = input_envelope[..., :min_length]
+    target_envelope = target_envelope[..., :min_length]
+    
+    return F.mse_loss(input_envelope, target_envelope)
+  
+if __name__ == "__main__":
+  pass
